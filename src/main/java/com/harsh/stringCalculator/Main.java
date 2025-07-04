ca1458b2ffb1404bf414795e434edee05f92ca1d
Harsh Manek <harshmanek786@gmail.com>
2025-07-04 08:19:00 +0530

    initialize project with Maven setup and basic structure

---
+++ src/main/java/com/harsh/stringCalculator/Main.java
@@ -0,0 +1,7 @@
+package com.harsh.stringCalculator;
+
+public class    Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/test/java/com/harsh/stringCalculator/StringCalculatorTest.java
@@ -0,0 +1,4 @@
+package com.harsh.stringCalculator;
+
+public class StringCalculatorTest {
+}
