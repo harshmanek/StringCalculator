ce76064a2e4a665b03353662dc50924cb843224d
Harsh Manek <harshmanek786@gmail.com>
2025-07-04 11:12:39 +0530

    support new line character in input and handle empty strings in addition method; add tests for multiple numbers and new line input

--- src/main/java/com/harsh/stringCalculator/StringCalculator.java
+++ src/main/java/com/harsh/stringCalculator/StringCalculator.java
@@ -9,15 +9,25 @@ public class StringCalculator {
         if (numbers.isEmpty()) {
             return 0;
         }
-        String[] numArray = numbers.split(",");
+//        we split the input string by comma or new line character
+//        and store the result in an array
+//        this gives us a list of
+        String[] numArray = numbers.split(",|\n");
+        for(String item : numArray) {
+            System.out.println("Item: " + item);
+        }
 //        for single number
         if (numArray.length == 1) {
             return Integer.parseInt(numArray[0]);
         }
 //        for more than one number
         int sum = 0 ;
-        for(String num:numArray){
-            sum+= Integer.parseInt(num);
+        for (String item : numArray) {
+            if (item.isEmpty()) {
+//        continue if the item in array is a empty string
+                continue;
+            }
+            sum += Integer.parseInt(item);
         }
 //        return the sum of numbers
         return sum;
--- src/test/java/com/harsh/stringCalculator/StringCalculatorTest.java
+++ src/test/java/com/harsh/stringCalculator/StringCalculatorTest.java
@@ -6,22 +6,38 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculatorTest {
 
-//    base test function for empty string
+    //    base test function for empty string
     @Test
-    void emptyStringReturnsZero(){
+    void emptyStringReturnsZero() {
         StringCalculator calculator = new StringCalculator();
         assertEquals(0, calculator.add(""));
     }
-//    base test function for single number
+
+    //    base test function for single number
     @Test
-    void stringWithOneNumberReturnsThatNumber(){
+    void stringWithOneNumberReturnsThatNumber() {
         StringCalculator calc = new StringCalculator();
         assertEquals(5, calc.add("5"));
     }
-//    test function for two numbers
+
+    //    test function for two numbers
     @Test
     void sumOfTwoNumbersReturnsTheirSum() {
         StringCalculator calc = new StringCalculator();
         assertEquals(10, calc.add("3,7"));
+    }
+
+    //test for multiple numbers
+    @Test
+    void sumOfMultipleNumbersReturnsTheirSum() {
+        StringCalculator calc = new StringCalculator();
+        assertEquals(15, calc.add("1,2,3,4,5"));
+    }
+
+    //    allow new line character in the input for strings
+    @Test
+    void newLineCharacterInInputReturnsSum() {
+        StringCalculator calc = new StringCalculator();
+        assertEquals(6, calc.add("1,2,\n3"));
     }
 }
